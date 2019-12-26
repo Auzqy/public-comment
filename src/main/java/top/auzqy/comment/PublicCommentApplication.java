cadb06010c8fa5a7a3cb1433ff16f31317c81cd5
au <auzhouqingyu@163.com>
2019-12-26 23:26:02 +0800

    project init

---
+++ src/main/java/top/auzqy/comment/PublicCommentApplication.java
@@ -0,0 +1,13 @@
+package top.auzqy.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicCommentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicCommentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/auzqy/comment/PublicCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package top.auzqy.comment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicCommentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

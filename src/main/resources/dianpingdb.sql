c937b253bfdbea1440d15295c0a433ba42ccf91d
au <auzhouqingyu@163.com>
2019-12-29 16:25:29 +0800

    add shop and category db related

--- src/main/resources/dianpingdb.sql
+++ src/main/resources/dianpingdb.sql
@@ -33,11 +33,6 @@ CREATE TABLE `user` (
   UNIQUE KEY `telphone_unique_index` (`telphone`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-SET FOREIGN_KEY_CHECKS = 1;
-
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for seller
@@ -50,6 +45,45 @@ CREATE TABLE `seller` (
   `updated_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
   `remark_score` decimal(2,1) NOT NULL DEFAULT '0.0',
   `disabled_flag` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `updated_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `icon_url` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `sort` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_unique_index` (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for shop
+-- ----------------------------
+DROP TABLE IF EXISTS `shop`;
+CREATE TABLE `shop` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `updated_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `name` varchar(80) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `remark_score` decimal(2,1) NOT NULL DEFAULT '0.0',
+  `price_per_man` int(11) NOT NULL DEFAULT '0',
+  `latitude` decimal(10,6) NOT NULL DEFAULT '0.000000',
+  `longitude` decimal(10,6) NOT NULL DEFAULT '0.000000',
+  `category_id` int(11) NOT NULL DEFAULT '0',
+  `tags` varchar(2000) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `start_time` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `end_time` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `address` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `seller_id` int(11) NOT NULL DEFAULT '0',
+  `icon_url` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
